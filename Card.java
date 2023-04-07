ae1298fa139ee1ca3b4e5328d3dccfa1a6d29410
Louis AB <louisamorosbessede@gmail.com>
2023-04-07 11:07:17 +0200

    changement de tom

--- Card.java
+++ Card.java
@@ -1,27 +1,6 @@
 import javax.persistence.*;
 import java.util.List;
 
-@Entity
-public class Card {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String title;
-    private String description;
-
-    @ElementCollection
-    private List<String> keywords;
-
-    @ManyToOne
-    @JoinColumn(name = "section_id")
-    private Section section;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    private String link;
 
     // Getters and Setters
 }
