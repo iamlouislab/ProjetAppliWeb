f4fccdaad883be26c3bd7e86f6a08916b2602c62
imlouislab <louisamorosbessede@gmail.com>
2023-05-12 10:10:21 +0200

    add login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,81 +1,73 @@
-import { useState } from "react";
-import Link from "next/link";
+import { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/authContext";
+import Router from "next/router";
 
-const AuthForm = () => {
-  const [email, setEmail] = useState("");
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isSignIn, setIsSignIn] = useState(true);
   const [error, setError] = useState("");
+  const authContext = useContext(AuthContext);
 
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
+    if (!authContext) {
+      setError("An error occurred. Please try again.");
+      return;
+    }
 
-  const handleAuth = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(
-      `email: ${email}, password: ${password}, isSignIn: ${isSignIn}`
-    );
-  };
+    const { setUser } = authContext;
 
-  const toggleAuthMode = () => {
-    setIsSignIn(!isSignIn);
-    setError("");
+    // Call your API
+    const res = await fetch("http://localhost:8080//login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      const { token, user } = await res.json();
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+      setUser(user);
+      // Redirect to home page
+      Router.push("/");
+    } else {
+      // Handle error
+      setError("Invalid username or password. Please try again.");
+    }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-1/3">
-        <h2 className="text-2xl font-bold mb-4">
-          {isSignIn ? "Sign in" : "Register"}
-        </h2>
-        <form onSubmit={handleAuth} className="flex flex-col space-y-4">
-          <label htmlFor="email" className="font-bold text-gray-700">
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={handleEmailChange}
-            className="rounded-lg border border-gray-400 px-4 py-2 focus:outline-none focus:border-blue-500"
-          />
-          <label htmlFor="password" className="font-bold text-gray-700">
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-            className="rounded-lg border border-gray-400 px-4 py-2 focus:outline-none focus:border-blue-500"
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          >
-            {isSignIn ? "Sign in" : "Register"}
-          </button>
-          {error && <p className="text-red-500">{error}</p>}
-        </form>
-        <p className="mt-4">
-          {isSignIn ? "Don't have an account?" : "Already have an account?"}
-          <span
-            className="text-blue-500 cursor-pointer ml-1 hover:underline"
-            onClick={toggleAuthMode}
-          >
-            {isSignIn ? "Register" : "Sign in"}
-          </span>
-        </p>
-      </div>
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <form onSubmit={handleSubmit} className="p-10 bg-white rounded shadow-xl w-1/2">
+        <h1 className="text-3xl font-bold mb-10 text-center">Login</h1>
+        {error && <p className="mb-4 text-red-500">{error}</p>}
+        <input
+          type="text"
+          className="block w-full p-2 mb-4 border rounded shadow-sm"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          className="block w-full p-2 mb-4 border rounded shadow-sm"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="block w-full p-2 text-white bg-blue-600 rounded hover:bg-blue-700"
+        >
+          Login
+        </button>
+      </form>
     </div>
   );
 };
 
-export default AuthForm;
+export default LoginPage;
