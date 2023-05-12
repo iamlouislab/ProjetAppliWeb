94c31de663669723f69a6388ba02ff830ade3806
imlouislab <louisamorosbessede@gmail.com>
2023-05-12 10:10:14 +0200

    add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import Navbar from "components/Navbar";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "../../contexts/authContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Navbar />
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </AuthProvider>
     </div>
   );
 }
