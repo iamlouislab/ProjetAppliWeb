9eac108a629cdfa1e80d416b4841c200d6484a20
imlouislab <louisamorosbessede@gmail.com>
2023-05-16 22:55:42 +0200

    user data hook

---
+++ hooks/useUserData.tsx
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import useSWR from "swr";
+import { AuthContext } from "../contexts/authContext";
+import Portfolio from "@/types/Portfolio";
+
+const useUserData = () => {
+  const authContext = useContext(AuthContext);
+
+  const fetchUserData = async () => {
+    if (!authContext?.user) {
+      console.log("No user found");
+      return;
+    }
+    console.log("Fetching user data...");
+
+    // Call your API
+    const res = await fetch(
+      "http://localhost:8080/ProjetAppliWeb/rest/users/getPortfolioByUserId/" +
+        authContext.user.id,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.ok) {
+      const { userData } = await res.json();
+      return userData as Portfolio;
+    } else {
+      console.log("error useUserPortfolio: " + error.message);
+      throw error;
+    }
+  };
+
+  const swrOptions = {
+    revalidateOnFocus: false, // Don't revalidate when the window gains focus
+    revalidateOnReconnect: false, // Don't revalidate when the network reconnects
+    dedupingInterval: 60 * 1000, // Deduplicate requests within 1 minute
+  };
+
+  const {
+    data: userData,
+    error,
+    isValidating,
+  } = useSWR(
+    authContext?.user ? ["user", authContext.user.id] : null,
+    fetchUserData,
+    swrOptions
+  );
+
+  const errorMessage = error ? error.message : "";
+
+  return { userData, isLoading: !userData && !error, errorMessage };
+};
+
+export default useUserData;
