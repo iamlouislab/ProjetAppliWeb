75de8cf5b0ee7745156705273f6886a1c202a587
imlouislab <louisamorosbessede@gmail.com>
2023-05-16 22:54:38 +0200

    set up component

---
+++ src/lib/utils.ts
@@ -0,0 +1,13 @@
+import { ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const baseColors = {
+  background_color: "white",
+  text_major_color: "black",
+  text_minor_2_color: "gray",
+  text_minor_color: "white"
+}
