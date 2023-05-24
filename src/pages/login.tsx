eff74f8705ce79a0ace0440f64f7fa09a0188f18
thierry <thierryxu01@gmail.com>
2023-05-24 16:09:36 +0200

    add new styles

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -70,94 +70,96 @@ const LoginCard: React.FC = () => {
   };
 
   return (
-    <div className="bg-white shadow-md rounded-lg p-6 w-1/2 mx-auto mt-10">
-      {formType === FormType.LOGIN ? (
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-      ) : (
-        <h2 className="text-2xl font-bold mb-4">Register</h2>
-      )}
-      {error && <p className="text-red-500 mb-4">{error}</p>}
-      <div className="mb-4">
-        <label
-          htmlFor="username"
-          className="block text-gray-700 font-bold mb-2"
-        >
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          className="border border-gray-400 p-2 w-full rounded"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="password"
-          className="block text-gray-700 font-bold mb-2"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="border border-gray-400 p-2 w-full rounded"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      {formType === FormType.REGISTER && (
+    <div className="bg-black h-screen">
+      <div className="bg-white shadow-md rounded-lg p-6 w-1/2 mx-auto mt-10">
+        {formType === FormType.LOGIN ? (
+          <h2 className="text-2xl font-bold mb-4">Login</h2>
+        ) : (
+          <h2 className="text-2xl font-bold mb-4">Register</h2>
+        )}
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <div className="mb-4">
           <label
-            htmlFor="description"
+            htmlFor="username"
             className="block text-gray-700 font-bold mb-2"
           >
-            Description
+            Username
           </label>
           <input
             type="text"
-            id="description"
+            id="username"
             className="border border-gray-400 p-2 w-full rounded"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-      )}
-      {formType === FormType.REGISTER && (
         <div className="mb-4">
           <label
-            htmlFor="githubLink"
+            htmlFor="password"
             className="block text-gray-700 font-bold mb-2"
           >
-            GitHub Link
+            Password
           </label>
           <input
-            type="text"
-            id="githubLink"
+            type="password"
+            id="password"
             className="border border-gray-400 p-2 w-full rounded"
-            value={githubLink}
-            onChange={(e) => setGithubLink(e.target.value)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-      )}
-      <div className="flex justify-between items-center">
-        <div
-          className="hover:underline cursor-pointer"
-          onClick={toggleFormType}
-        >
-          {formType === FormType.LOGIN
-            ? "No account yet? Register"
-            : "Already have an account? Login"}
+        {formType === FormType.REGISTER && (
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Description
+            </label>
+            <input
+              type="text"
+              id="description"
+              className="border border-gray-400 p-2 w-full rounded"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+        )}
+        {formType === FormType.REGISTER && (
+          <div className="mb-4">
+            <label
+              htmlFor="githubLink"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              GitHub Link
+            </label>
+            <input
+              type="text"
+              id="githubLink"
+              className="border border-gray-400 p-2 w-full rounded"
+              value={githubLink}
+              onChange={(e) => setGithubLink(e.target.value)}
+            />
+          </div>
+        )}
+        <div className="flex justify-between items-center">
+          <div
+            className="hover:underline cursor-pointer"
+            onClick={toggleFormType}
+          >
+            {formType === FormType.LOGIN
+              ? "No account yet? Register"
+              : "Already have an account? Login"}
+          </div>
+          <button
+            className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={formType === FormType.LOGIN ? handleLogin : handleRegister}
+          >
+            {formType === FormType.LOGIN ? "Login" : "Register"}
+          </button>
         </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          onClick={formType === FormType.LOGIN ? handleLogin : handleRegister}
-        >
-          {formType === FormType.LOGIN ? "Login" : "Register"}
-        </button>
       </div>
-    </div>
+      </div>
   );
 };
 
